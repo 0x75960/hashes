16c6252d23b5befb2ebdc7764b2e7b3cbb281f9e
Hiromitsu Oshiba <h.oshiba.0117@gmail.com>
2019-07-08 22:25:02 +0900

    :sparkles: improve error handling

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use std::io::prelude::*;
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut buffer = String::new();
-    std::io::stdin().read_to_string(&mut buffer);
+    std::io::stdin().read_to_string(&mut buffer)?;
     midy::find(&buffer).into_iter().for_each(|x| println!("{}", x));
+    Ok(())
 }
